8be5671967686fd69df66e95bc882352d4d90a23
Rahulrkr1996 <rahul.kumarrkr96@gmail.com>
2015-05-25 06:40:31 +0530

    changes made, code working

--- encoder/src/devices.h
+++ encoder/src/devices.h
@@ -1,9 +1,9 @@
 #define IMU_COM_PORT "/dev/ttyUSB2"
 #define IMU_BAUD_RATE 57600
 
-#define BOT_COM_PORT "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_754393336353517140A0-if00"
-#define BOT_BAUD_RATE 19200
+#define BOT_COM_PORT "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_7403431393835191A1E1-if00"
+#define BOT_BAUD_RATE 28800
 
-#define ENCODER_COM_PORT "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_754393336353517140A0-if00"
-#define ENCODER_BAUD_RATE 19200
+#define ENCODER_COM_PORT "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_7403431393835191A1E1-if00"
+#define ENCODER_BAUD_RATE 28800
 #define GPS_COM_PORT "/dev/ttyUSB0"
--- encoder/src/encoder.cpp
+++ encoder/src/encoder.cpp
@@ -16,24 +16,70 @@
 		
 		returnValue.leftCount = 0;
 		returnValue.rightCount = 0;
-		
+		int checker=0;
+		ROS_INFO("inside fetch\n");
+
+        input = serialConnection->getChar();
+        ROS_INFO("char : %c\n",input);
+        if(input==108)
+        {ROS_INFO("inside l\n");
 		do {
+		     
 			input = serialConnection->getChar();
-			if ( (input < 58 && input > 47)  || (input==45)) {
+			ROS_INFO("char : %c\n",input);
+		      
+		      if(input==45)
+			{
+			   checker=1;
+			}
+			
+			else if ( (input < 58 && input > 47) ) 
+			{
 				returnValue.leftCount *= 10;
 				returnValue.leftCount += input - 48;
+				ROS_INFO("inside left\n");
 			}
+		      
 		} while (input != ' ');
-		
+	
+		    
+		if(checker==1)
+		returnValue.leftCount=(0 - returnValue.leftCount ) ;
+
+		checker=0;
+		}
+		if(input==' ')
+		{input = serialConnection->getChar();
+		ROS_INFO("char : %c\n",input);}
+		if(input==114)
+        {
 		do {
+		     
 			input = serialConnection->getChar();
-			if ((input < 58 && input > 47 ) || (input==45)) {
+			ROS_INFO("char : %c\n",input);
+		      
+		      if(input==45)
+			{
+			   checker=1;
+			}
+			
+			else if ( (input < 58 && input > 47) ) 
+			{
 				returnValue.rightCount *= 10;
 				returnValue.rightCount += input - 48;
+				ROS_INFO("inside right\n");
+				
 			}
+		      
 		} while (input != ' ');
+	
+
+		if(checker==1)
+		returnValue.rightCount=(0 - returnValue.rightCount ) ;
+		}
 		
 		return returnValue;
+
 		
 	}
 	
